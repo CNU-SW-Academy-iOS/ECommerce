3c258e98d73c8527ebe16fae3da25ebf1dcac5bb
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2023-09-24 20:58:02 +0900

    Initial Commit

---
+++ ECommerce/ECommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceApp.swift
+//  ECommerce
+//
+//  Created by CNU on 2023/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
